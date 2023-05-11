09787c43e5ab6bc5108147c3daf07afacd1dbb0c
lucasamazonas <lucassilvaamazonas@gmail.com>
2023-05-11 08:02:59 -0400

    criação de token

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use jwt_simple::prelude::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    let key = HS256Key::generate();
+
+    let claims = Claims::create(Duration::from_hours(2));
+    let token = key.authenticate(claims).unwrap();
+    let token_is_valid = key.verify_token::<NoCustomClaims>(&token, None);
+
+    println!("{token}");
+
+    match token_is_valid {
+        Ok(_) => println!("Token válido"),
+        _ => println!("token inválido!")
+    }
 }
